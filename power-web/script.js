49e51644bc7751775f6de1e69aae8281e8a45f85
teamigl <anakminecraft8@gmail.com>
2024-11-14 00:23:58 +0530

    Delete power-web directory

--- power-web/script.js
+++
@@ -1,8 +0,0 @@
-// Redirect to landing page after 5 seconds on the onboarding page
-window.addEventListener('DOMContentLoaded', () => {
-    if (window.location.pathname.includes("onboarding.html")) {
-        setTimeout(() => {
-            window.location.href = "index.html"; // Redirect to the landing page
-        }, 5000); // 5000 ms = 5 seconds
-    }
-});
